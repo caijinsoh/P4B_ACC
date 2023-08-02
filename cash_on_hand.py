41d18eb0171841e2282c54a9c34b12622650d9c8
Jess <jesslim35@gmail.com>
2023-08-02 11:34:49 +0800

    igp

---
+++ cash_on_hand.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import csv
+
+def compute_cash_difference(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header
+
+        cash_on_hand = []
+
+        for row in reader:
+            cash_on_hand.append([int(row[0]), int(row[1])])
+
+    deficit_cash = []
+    highest_surplus = 0
+    highest_surplus_day = None
+
+    for i in range(1, len(cash_on_hand)):
+        day, cash = cash_on_hand[i]
+        prev_day, prev_cash = cash_on_hand[i - 1]
+        difference = cash - prev_cash
+
+        if difference < 0:
+            deficit_cash.append((day, abs(difference)))  # Use abs() to make it positive
+        else:
+            if difference > highest_surplus:
+                highest_surplus = difference
+                highest_surplus_day = day
+
+    return deficit_cash, highest_surplus, highest_surplus_day
---
+++ profit_and_loss.py
@@ -0,0 +1,33 @@
+import csv
+
+def compute_profit_difference(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip header
+
+        previous_profit = 0
+        profit_decrease_details = []
+        highest_increase = 0
+        highest_increase_day = None
+
+        for row in reader:
+            day, profit = int(row[0]), int(row[1])
+
+            if profit < previous_profit:
+                difference = previous_profit - profit
+                profit_decrease_details.append((day, difference))
+            else:
+                increment = profit - previous_profit
+                if increment > highest_increase:
+                    highest_increase = increment
+                    highest_increase_day = day
+
+            previous_profit = profit
+
+        return profit_decrease_details, highest_increase, highest_increase_day
+compute_profit_difference("profitsandloss.csv")
+
+
+
+
+
