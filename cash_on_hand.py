e030f0f9b57380fcab7d13622470ad36a06ef642
cryscross <crystaltan6767@gmail.com>
2023-08-07 16:00:33 +0800

    hihihih

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -24,3 +24,16 @@ def compute_cash_differnce(file_path):
         prev_day, prev_cash = cash_on_hand[i - 1]
         difference = cash - prev_cash
         
+        # If the difference is negative, it means there is a deficit cash on that day.
+        # Append the day and the absolute value of the difference to the deficit_cash list.
+        if difference < 0:
+            deficit_cash.append((day, abs(difference)))  # Use abs() to make it positive
+        else:
+            # If the difference is positive, update the highest_surplus and highest_surplus_day
+            # if the current difference is greater than the previous highest_surplus.
+            if difference > highest_surplus:
+                highest_surplus = difference
+                highest_surplus_day = day
+
+    # Return the deficit_cash list, highest_surplus amount, and the day with the highest surplus.
+    return deficit_cash, highest_surplus, highest_surplus_day
