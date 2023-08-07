52e677282e167af2d9697064a30d437c16b15608
Jess-JT <jesslim35@gmail.com>
2023-08-07 15:29:26 +0800

    edited

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,14 +1,14 @@
 from pathlib import Path 
 import csv
 
-def compute_cash_difference(file_path):
-     # Read data from the CSV file and store it in a list called cash_on_hand.
-    with open(file_path, 'r') as file:
+def compute_cash_differnce(file_path):
+    # Read data from the CSV file and store it in an empty list-cash_on_hand.
+    with open (file_path,"r") as file:
         reader = csv.reader(file)
-        next(reader)  # Skip header
+        next(reader) # Skip reader
 
         cash_on_hand = []
-
+        
         for row in reader:
             cash_on_hand.append([int(row[0]), int(row[1])])
 
@@ -18,13 +18,13 @@ def compute_cash_difference(file_path):
     highest_surplus = 0
     highest_surplus_day = None
 
-     # Loop through the cash_on_hand list to calculate the deficit cash and find the highest surplus.
+    # Loop through the cash_on_hand list to calculate the dificit cash and find the highest surplus.
     for i in range(1, len(cash_on_hand)):
         day, cash = cash_on_hand[i]
         prev_day, prev_cash = cash_on_hand[i - 1]
         difference = cash - prev_cash
         
-         # If the difference is negative, it means there is a deficit cash on that day.
+        # If the difference is negative, it means there is a deficit cash on that day.
         # Append the day and the absolute value of the difference to the deficit_cash list.
         if difference < 0:
             deficit_cash.append((day, abs(difference)))  # Use abs() to make it positive
